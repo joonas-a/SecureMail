f6ac3c6b5278e4a43fc1d060353a5c742fdec935
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-09-25 23:56:18 +0300

    add message view

--- secure_mail/urls.py
+++ secure_mail/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index, login
+from .views import index, messages
 
 urlpatterns = [
     path("", index, name="index"),
+    path("messages/<str:username>/", messages, name="messages"),
 ]
--- secure_mail/views.py
+++ secure_mail/views.py
@@ -1,15 +1,22 @@
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.template import loader
+from secure_mail.models import Message
 # Create your views here.
+
+mockdata = ["asdkjjksdf", "asdffksldj", "lkl,mnsfkjd", "lkvcxjkldf"]
 
 
 @login_required
 def index(request):
     print("User is authenticated: ", request.user.is_authenticated)
     return render(request, 'index.html')
+
+
+def messages(request, username):
+    data = Message.objects.filter(receiver=username)
+    context = {"messages": data, "username": username}
+    return render(request, 'messages.html', context)
 
 
 def logout(request):
